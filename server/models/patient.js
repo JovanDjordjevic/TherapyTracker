a8dea1026df89356033a20caffd71be21b987f79
mi17167 <petar.rondovic78@gmail.com>
2021-11-28 14:11:55 +0100

    Make database model for patients

--- server/models/patient.js
+++ server/models/patient.js
@@ -1,5 +1,57 @@
 const mongoose = require('mongoose');
 
-// shema za pacijenta
+const patientsSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    jmbg: {
+        type: String,
+        require: true,
+    },
+    name: {
+        type: String,
+        require: true,
+    },
+    parentName: {
+        type: String,
+        require: true,
+    },
+    surname: {
+        type: String,
+        require: true,
+    },
+    yearOfBirth: {
+        type: Number,
+        require: true,
+    },
+    //Pol (m ili z vrednost, bira se preko radio dugmica)
+    gender: {
+        type: String,
+        require: true,
+    },
+    address: {
+        type: String,
+        require: true,
+    },
+    city: {
+        type: String,
+        require: true,
+    },
+    contact: {
+        type: String,
+        require: true,
+    },
+    email: String,
+    tumorDateDiagnosis: {
+        type: Date,
+        require: true,
+    },
+    //veca niska
+    familyAnamnesis: {
+        type: String,
+        require: true,
+    },
+    
+});
 
-// export shema
+const patientsModel = mongoose.model('patients', patientsSchema);
+
+module.exports = patientsModel;
