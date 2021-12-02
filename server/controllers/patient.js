5f913187ddb9af475a294592bb8f397da60ea9cc
mi17167 <petar.rondovic78@gmail.com>
2021-12-02 17:15:03 +0100

    Implement some database requests
    - Implement getAllPatients
    - Implement getPatientByName
    - Implement addNewPatient
    - Implement deletePatient

--- server/controllers/patient.js
+++ server/controllers/patient.js
@@ -1,20 +1,99 @@
-const mongoose = require('mongoose');
-const Patient = require('../models/patient');
+const patientsService = require('../services/patients');
 
-const getAllPatients = async (rec, res, next) => {
-    // console.log('getAllPatients');
+const getAllPatients = async (req, res, next) => {
+    try {
+        const allPatients = await patientsService.getAllPatients();
+        res.status(200).json(allPatients);
+    } catch (error) {
+        next(error);
+    }
 };
 
 const getPatientByName = async (rec, res, next) => {
-    // console.log('getPatientByName');
+    const name = req.params.name;
+
+    try {
+        if (name == undefined) {
+            const error = new Error('Name missing!');
+            error.status = 400;
+            throw error;
+        }
+
+        const patient = await patientsService.getPatientByName(name);
+        if (patient == null) {
+            res.status(404).json();
+        } else {
+            res.status(200).json(patient);
+        }
+    } catch (error) {
+        next(error);
+    }
 };
 
-const addNewPatient = async (rec, res, next) => {
-    // console.log('addNewPatient');
+const addNewPatient = async (req, res, next) => {
+    const { jmbg, name, parentName, surname, yearOfBirth, gender, address,
+        city, contact, email, tumorDateDiagnosis, familyAnamnesis } = req.body;
+    try {
+        if (
+            !jmbg ||
+            !name ||
+            !parentName ||
+            !surname ||
+            !yearOfBirth ||
+            !gender ||
+            !address ||
+            !city ||
+            !contact ||
+            !tumorDateDiagnosis ||
+            !familyAnamnesis
+        ) {
+            const error = new Error('Check input data!');
+            error.status = 400;
+            throw error;
+        }
+
+        const newPatinet = await patientsService.addNewPatinet(
+            jmbg,
+            name,
+            parentName,
+            surname,
+            yearOfBirth,
+            gender,
+            address,
+            city,
+            contact,
+            email,
+            tumorDateDiagnosis,
+            familyAnamnesis
+        );
+        res.status(201).json(newPatinet);
+    } catch (error) {
+        next(error);
+    }
 };
 
-const deletePatient = async (rec, res, next) => {
-    // console.log('deletePatient');
+const deletePatient = async (req, res, next) => {
+    const jmbg = req.params.jmbg;
+
+    try {
+        if (!jmbg) {
+            const error = new Error('Jmbg missing!');
+            error.status = 400;
+            throw error;
+        }
+
+        const patient = await patientsService.getUserByJmbg(jmbg);
+        if (!patient) {
+            const error = new Error('Check jmbg!');
+            error.status = 404;
+            throw error;
+        }
+
+        await patientsService.deletePatient(jmbg);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = {
