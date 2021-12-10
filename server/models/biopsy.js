47f55de5a2ebaf3272b29a1d3c5f945ca057a24c
mi17167 <petar.rondovic78@gmail.com>
2021-12-10 22:45:58 +0100

    Implement basic tumor model

--- server/models/biopsy.js
+++ server/models/biopsy.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const biopsySchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    date: {
+    dateOfBiopsy: {
         type: Date,
         require: true,
     },
-    side: {
+    biopsySide: {
         type: String,
         require: true,
     },
--- server/models/tumor.js
+++ server/models/tumor.js
@@ -1,2 +1,27 @@
 const mongoose = require('mongoose');
 
+const tumorSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    T_stage: {
+        type: String,
+        require: true,
+    },
+    N_stage: {
+        type: Number,
+        require: true,
+        enum: [0, 1, 2, 3],
+    },
+    M_stage: {
+        type: Number,
+        require: true,
+        enum: [0, 1],
+    },
+    clinicalStage: {
+        type: String,
+        require: true,
+    }
+});
+
+//const tumorModel = mongoose.model('tumor', tumorSchema);
+
+module.exports = tumorSchema;
