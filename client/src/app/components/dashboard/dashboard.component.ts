db92de20becc0c491a4b8857f03b19e3f43f172d
unknown <selena.hocevar@gmail.com>
2021-12-28 04:34:09 +0100

    Updated patient form

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  displayPatientForm: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  setShowPatientForm(value: boolean) {
+    this.displayPatientForm = value;
   }
 
+  onNewPatient() {
+    this.displayPatientForm = true;
+  }
+
+  ngOnInit(): void {}
 }
--- client/src/app/components/patient-form/patient-form.component.ts
+++ client/src/app/components/patient-form/patient-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Patient } from 'src/app/models/patient.model';
+import { Gender, Menopause } from 'src/app/models/patient.model';
+import { Output, EventEmitter } from '@angular/core';
+
+declare const $: any;
 
 @Component({
   selector: 'app-patient-form',
@@ -8,10 +13,35 @@ import { Patient } from 'src/app/models/patient.model';
 })
 export class PatientFormComponent implements OnInit {
   shouldDisplayMenopauseForm: boolean = false;
+  patientForm: FormGroup;
+  Gender = Gender;
+  Menopause = Menopause;
+  @Output() onDisplayPatientForm = new EventEmitter<boolean>();
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {
+    this.patientForm = this.formBuilder.group({
+      jmbg: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      parentName: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      yearOfBirth: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      menopause: ['', [Validators.required]],
+      address: ['', []],
+      city: ['', [Validators.required]],
+      contact: ['', [Validators.required]],
+      email: ['', []],
+      tumorDateDiagnosis: ['', [Validators.required]],
+      familyAnamnesis: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {}
+
+  onPatientFormSubmit() {
+    //console.log(this.patientForm);
+    this.onDisplayPatientForm.emit(false);
+  }
 
   onFemaleChecked() {
     this.shouldDisplayMenopauseForm = true;
