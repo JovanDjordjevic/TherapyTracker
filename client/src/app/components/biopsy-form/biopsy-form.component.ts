2ab207f9fd544bd89f626b67803038ce10615335
Jovan Djordjevic <joca.dj@hotmail.com>
2022-01-07 16:52:14 +0100

    Fix biopsy form bug
    Fixed bug where user cannot input a biopsy into the DB if he first does not input a valid date and then fixes it to be correct

--- client/src/app/components/biopsy-form/biopsy-form.component.ts
+++ client/src/app/components/biopsy-form/biopsy-form.component.ts
@@ -30,22 +30,18 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
   dateHasErrors: boolean = false;
   sideHasErrors: boolean = false;
   biopsyTypeLeftHasErrors: boolean = false;
-  numLeftHasErrors: boolean = false;
   histotypeLeftHasErrors: boolean = false;
   multifocalityLeftHasErrors: boolean = false;
   biopsyTypeRightHasErrors: boolean = false;
-  numRightHasErrors: boolean = false;
   histotypeRightHasErrors: boolean = false;
   multifocalityRightHasErrors: boolean = false;
 
   dateErrors: string[] = [];
   sideErrors: string[] = [];
   biopsyTypeLeftErrors: string[] = [];
-  numLeftErrors: string[] = [];
   histotypeLeftErrors: string[] = [];
   multifocalityLeftErrors: string[] = [];
   biopsyTypeRightErrors: string[] = [];
-  numRightErrors: string[] = [];
   histotypeRightErrors: string[] = [];
   multifocalityRightErrors: string[] = [];
 
@@ -62,11 +58,9 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
       // za polja ispod se ne koriste na ovom mestu validatori zbog nacina kako se formular ponasa
       // validacija se radi u okviru pomocnih funkcija u okviru ovog fajla
       biopsyTypeLeft: ['', []],
-      numLeft: ['', []],
       histotypeLeft: ['', []],
       multifocalityLeft: ['', []],
       biopsyTypeRight: ['', []],
-      numRight: ['', []],
       histotypeRight: ['', []],
       multifocalityRight: ['', []],
       comment: ['', []],
@@ -83,7 +77,6 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
   }
 
   onBiopsyFormSubmit() {
-    //this.biopsyService.getAllBiopsies(1).subscribe((data) => console.log(data));
     if (this.biopsyForm.invalid) {
       window.alert('Neka polja nemaju validnu vrednost!');
       // za 2 ugradjena validatora
@@ -91,14 +84,19 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
       this.updateSideErrors();
       return;
     }
+    else {
+      this.dateHasErrors = false;
+      this.dateErrors = [];
+      this.sideHasErrors = false;
+      this.sideErrors = [];
+    }
 
     // validatori iz ovog fajla
     this.updateLeftAndRightErrors();
 
 
-    if (this.dateHasErrors == true || this.sideHasErrors == true ||
-      this.biopsyTypeLeftHasErrors == true || this.numLeftHasErrors == true || this.histotypeLeftHasErrors == true || this.multifocalityLeftHasErrors == true ||
-      this.biopsyTypeRightHasErrors == true || this.numRightHasErrors == true || this.histotypeRightHasErrors == true || this.multifocalityRightHasErrors == true) {
+    if (this.biopsyTypeLeftHasErrors == true || this.histotypeLeftHasErrors == true || this.multifocalityLeftHasErrors == true ||
+        this.biopsyTypeRightHasErrors == true || this.histotypeRightHasErrors == true || this.multifocalityRightHasErrors == true) {
       window.alert('Neka polja nemaju validnu vrednost!');
       return;
     }
@@ -106,11 +104,9 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
     this.dateHasErrors = false;
     this.sideHasErrors = false;
     this.biopsyTypeLeftHasErrors = false;
-    this.numLeftHasErrors = false;
     this.histotypeLeftHasErrors = false;
     this.multifocalityLeftHasErrors = false;
     this.biopsyTypeRightHasErrors = false;
-    this.numRightHasErrors = false;
     this.histotypeRightHasErrors = false;
     this.multifocalityRightHasErrors = false;
 
@@ -164,23 +160,19 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
     switch (selectedSide) {
       case BiopsySide.Left:
         this.updateBiopsyTypeLeftErrors();
-        //this.updateNumLeftErrors();
         this.updateHistotypeLeftErrors();
         this.updateMultifocalityLeftErrors();
         break;
       case BiopsySide.Right:
         this.updateBiopsyTypeRightErrors();
-        //this.updateNumRightErrors();
         this.updateHistotypeRightErrors();
         this.updateMultifocalityRightErrors();
         break;
       case BiopsySide.Both:
         this.updateBiopsyTypeLeftErrors();
-        //this.updateNumLeftErrors();
         this.updateHistotypeLeftErrors();
-        this.updateMultifocalityLeftErrors(); 8
+        this.updateMultifocalityLeftErrors();
         this.updateBiopsyTypeRightErrors();
-        //this.updateNumRightErrors();
         this.updateHistotypeRightErrors();
         this.updateMultifocalityRightErrors();
         break;
@@ -201,18 +193,6 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
     }
     else {
       this.biopsyTypeLeftHasErrors = false;
-    }
-  }
-
-  updateNumLeftErrors() {
-    this.numLeftErrors = [];
-    const value: string = this.biopsyForm.get('numLeft')?.value;
-    if (this.checkIfBiopsyNumberIsValid(value)) {
-      this.numLeftHasErrors = false;
-    }
-    else {
-      this.numLeftHasErrors = true;
-      this.numLeftErrors.push("Broj biopsije mora biti u formatu broj/godina");
     }
   }
 
@@ -252,18 +232,6 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
     }
   }
 
-  updateNumRightErrors() {
-    this.numRightErrors = [];
-    const value: string = this.biopsyForm.get('numRight')?.value;
-    if (this.checkIfBiopsyNumberIsValid(value)) {
-      this.numRightHasErrors = false;
-    }
-    else {
-      this.numRightHasErrors = true;
-      this.numRightErrors.push("Broj biopsije mora biti u formatu broj/godina");
-    }
-  }
-
   updateHistotypeRightErrors() {
     this.histotypeRightErrors = [];
     const value: string = this.biopsyForm.get('histotypeRight')?.value;
@@ -288,15 +256,10 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
     }
   }
 
-  checkIfBiopsyNumberIsValid(data: string): boolean {
-    if (!data.match(new RegExp("^[0-9]+/[0-9]+$"))) {
-      return false;
-    }
-    return true;
-  }
-
   checkIfMultifocalityIsValid(data: string): boolean {
-    if (!data.match(new RegExp("^ne|[0-9]+"))) {
+    const regex1 = new RegExp("^ne$");
+    const regex2 = new RegExp("^[0-9]+$"); 
+      if (!data.match(regex1) && !data.match(regex2)) {
       return false;
     }
     return true;
@@ -306,11 +269,9 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
     this.leftFormDisabled = false;
     this.rightFormDisabled = true;
     // moraju da se ociste vrednsoti formulara i errori kada neka strana nije vise potrebna
-    this.biopsyForm.patchValue({ biopsyTypeRight: '', numRight: '', histotypeRight: '', multifocalityRight: '' });
+    this.biopsyForm.patchValue({ biopsyTypeRight: '', histotypeRight: '', multifocalityRight: '' });
     this.biopsyTypeRightHasErrors = false;
     this.biopsyTypeRightErrors = [];
-    this.numRightHasErrors = false;
-    this.numRightErrors = [];
     this.histotypeRightHasErrors = false;
     this.histotypeRightErrors = [];
     this.multifocalityRightHasErrors = false;
@@ -321,11 +282,9 @@ export class BiopsyFormComponent implements OnInit, OnDestroy {
     this.rightFormDisabled = false;
     this.leftFormDisabled = true;
     // moraju da se ociste vrednsoti formulara i errori kada neka strana nije vise potrebna
-    this.biopsyForm.patchValue({ biopsyTypeLeft: '', numLeft: '', histotypeLeft: '', multifocalityLeft: '' });
+    this.biopsyForm.patchValue({ biopsyTypeLeft: '', histotypeLeft: '', multifocalityLeft: '' });
     this.biopsyTypeLeftHasErrors = false;
     this.biopsyTypeLeftErrors = [];
-    this.numLeftHasErrors = false;
-    this.numLeftErrors = [];
     this.histotypeLeftHasErrors = false;
     this.histotypeLeftErrors = [];
     this.multifocalityLeftHasErrors = false;
