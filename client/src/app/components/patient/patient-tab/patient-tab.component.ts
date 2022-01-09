ab828d3b1a9ce810724a4645148f9c3841cfb199
mi17167 <petar.rondovic78@gmail.com>
2022-01-09 16:51:19 +0100

    Generate pdf from patient info

--- client/src/app/components/patient/patient-tab/patient-tab.component.ts
+++ client/src/app/components/patient/patient-tab/patient-tab.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, EventEmitter, Output } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Gender, Menopause, Patient } from 'src/app/models/patient.model';
 import { CommonService } from 'src/app/services/common.service';
 import { PatientService } from 'src/app/services/patient-service.service';
-
+import html2canvas from 'html2canvas';
+import {jsPDF} from 'jspdf';
 @Component({
   selector: 'app-patient-tab',
   templateUrl: './patient-tab.component.html',
@@ -15,6 +16,8 @@ export class PatientTabComponent implements OnInit , OnDestroy{
   showPatientInfo: boolean = true;
   showClinicalStateForm: boolean = false;
   sub : Subscription = new Subscription();
+
+  //@Output() genPDF2: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(private patientService: PatientService, private commonService : CommonService) {
     //this.patient = new Patient('a','a','a','a',0,Gender.Female, Menopause.Peri, '',  '', '',  '', new Date(), ''  );
@@ -44,5 +47,26 @@ export class PatientTabComponent implements OnInit , OnDestroy{
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
+  }
+
+  genPDF() {
+    const patientInfoElement: HTMLElement | null = document.getElementById("patientInfoId");
+    
+    html2canvas(patientInfoElement as HTMLElement).then(function (canvas) {
+      var img = canvas.toDataURL("image/png")
+
+      const doc = new jsPDF();
+      doc.addImage(img, 'PNG', 19, 13, 172, 135);
+      
+      const clinicalStateElement: HTMLElement | null = document.getElementById("clinicalStateId");
+      html2canvas(clinicalStateElement as HTMLElement).then(function (canvas) {
+        var img = canvas.toDataURL("image/png")
+
+        doc.addImage(img, 'PNG', 19, 158, 172, 56);
+        doc.save("a4.pdf");
+
+      })
+    });
+    
   }
 }
